ed69e058ed9282239d1bfa7a72cc2972edad1850
Neely <rrniii@gmail.com>
2026-06-26 18:13:31 +0200

    Update package description

--- src/uk_wsr_visualizer/__init__.py
+++ src/uk_wsr_visualizer/__init__.py
@@ -1,4 +1,4 @@
-"""UK radar WCT-style toolkit for UK WSR Visualizer radar aggregates."""
+"""Quick-look tools for UK weather surveillance radar aggregate HDF5 data."""
 
 __all__ = ["__version__"]
 __version__ = "0.1.0"
